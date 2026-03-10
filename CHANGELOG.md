857a5702acce5c68b83ebdb2a1e93be4a6fe7e1e
Felix Fennell <felnne@bas.ac.uk>
2026-03-10 12:05:32 +0000

    Add output for cloudfront distribution ID
    For use in Lantern invalidation dev task.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+
+* Output for CloudFront distribution ID
+
 ### Changed
 
 * Using a `aws_iam_policy_document` data block to set S3 bucket policy
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "s3_bucket_name" {
   description = "S3 bucket name"
   value       = aws_s3_bucket.this.bucket
 }
+
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID"
+  value       = aws_cloudfront_distribution.this.id
+}
